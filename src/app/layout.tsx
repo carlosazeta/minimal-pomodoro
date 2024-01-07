568524f48fb1efdf30662dad3797d10f1f9fe546
carlosazeta <carlosagundezdev@gmail.com>
2024-01-07 23:18:26 +0100

    Install shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import SideNav from './components/sidenav'
+import SideNav from '../components/sidenav'
 
 const inter = Inter({ subsets: ['latin'] })
 
